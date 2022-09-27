ee11aa94be74a58c01d0ffc04a2a2c7120c8aff9
Renner Costa <86391973+rennerCostaa12@users.noreply.github.com>
2022-09-26 23:30:43 -0300

    arquivo env retirado

--- src/config/firebase/config.jsx
+++ src/config/firebase/config.jsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MENSSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+    apiKey: "AIzaSyBplXoy5kNJZo2oHR76zhMnpOPZnV9H-TM",
+    authDomain: "refinance-a2d3a.firebaseapp.com",
+    projectId: "refinance-a2d3a",
+    storageBucket: "refinance-a2d3a.appspot.com",
+    messagingSenderId: "659929011751",
+    appId: "1:659929011751:web:4a4b8be4c83f1492a1a002"
 };
 
 export const AppFirebase = initializeApp(firebaseConfig);
